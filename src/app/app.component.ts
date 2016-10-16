598d850a79c43eff13b239c1521cfb7f5e8e57eb
Fabrice Ginape <fabriceg@vifm.lcl>
2016-10-17 10:10:06 +1100

    Demonstration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,9 +31,9 @@ export class ConferenceApp {
   // the left menu only works after login
   // the login page disables the left menu
   appPages: PageObj[] = [
-    { title: 'Schedule', component: TabsPage, icon: 'calendar' },
+    { title: 'Body Collection', component: TabsPage, icon: 'calendar' },
     { title: 'Drivers', component: TabsPage, index: 1, icon: 'contacts' },
-    { title: 'Map', component: TabsPage, index: 2, icon: 'map' },
+    { title: 'Driver', component: TabsPage, index: 2, icon: 'map' },
     { title: 'About', component: TabsPage, index: 3, icon: 'information-circle' },
   ];
   loggedInPages: PageObj[] = [
